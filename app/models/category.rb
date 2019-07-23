b7186c773854c423d3a6db6bb57e6a87a6a05227
github username <github email address>
2019-07-23 11:34:25 -0400

    Done.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ActiveRecord::Base
+    has_many :posts
 end
+
+# Since the category has many posts./...when you go to the show page you can find and list 
+#All the posts associated with that category 
+
